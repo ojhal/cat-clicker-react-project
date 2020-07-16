3d434f18bfc8d0f9f9182049c167e1e0325552d5
Ojhal Patwa <patkarojhal26@gmail.com>
2020-07-16 15:07:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+import React, { Component } from "react";
+
+export default class App extends Component {
+  handleClick = () => {
+    console.log("button-clicked", this);
+  }
+  render() {
+    return (
+      <div>
+        <button className="btn btn-success" onClick={this.handleClick}>click</button>
+      </div>
+    )
+  }
+} 
